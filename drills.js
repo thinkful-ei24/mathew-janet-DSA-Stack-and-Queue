519004854f63620d668df8ecf9c421845d8d61fd
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-09 12:18:51 -0500

    Peek and Display are done

--- drills.js
+++ drills.js
@@ -36,13 +36,53 @@ class Stack {
     this.top = node.next;
     return node.data;
   }
-  
+}
+
+function peek(stack) {
+  return stack.top.data;
+}
+
+function display(stack) {
+  //input: 5, 4, 3
+  //output: 5...4...top is 3
+
+  //create a new stack
+  //loop through old stack
+  //  push each value from old stack to new stack
+  //  stop when top == null
+  // new stack is 3, 4, 5
+  //loop through new stack
+  //  pop item from new stack
+  //  concat to string (num...) (each popped value)
+  // print: 5, 4, 3
+  const newStack = new Stack();
+  let outputString = '';
+  let tempPosition = stack.top;
+
+
+  while (tempPosition) {
+    // push tempPosition.data into newStack
+    // save stack.next into tempPosition
+    newStack.push(tempPosition.data);
+    tempPosition = tempPosition.next;
+  }
+
+  while (newStack.top) {
+    outputString += newStack.pop() + '...';
+  }
+
+  outputString += 'finished!!!';
+  return outputString;
 }
 
 const stack = new Stack();
 stack.push(5);
 stack.push(4);
 stack.push(3);
-console.log(stack.pop());
-console.log(stack);
+// console.log(stack.pop());
+// console.log(peek(stack))
+console.log(display(stack))
+// display(stack)
+// console.log(stack);
+
 
